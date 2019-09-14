e35a20f63e0db74a3c0d5c3ec6aded1b2a6e82b0
Ksenia Sukhova <ks.sukhova@gmail.com>
2019-09-14 11:54:27 +0300

    Доп задание)

--- price.py
+++ price.py
@@ -10,7 +10,7 @@ print(sum_string)
 
 def format_price(price):
     price = int(price)
-    return "Price: " + str(price) + " rub."
+    return "Price: " + str(price) + " rub." # попробуй разобраться и сделать через .format()
 display_price = format_price(56.24)
 print(display_price)
 
@@ -18,7 +18,7 @@ print(display_price)
 # def discounted(price, discount, max_discount=50):
     # price = abs(float(price))
     # discount = abs(float(discount))
-    # max_discount = abs(float(discount))
+    # max_discount = abs(float(discount))#снова дискаунт а не макс дискаунт 
     # if max_discount > 99:
     #    raise ValueError("Max discount cannot be more then 99%")
     #if discount >= max_discount:
@@ -27,6 +27,7 @@ print(display_price)
         #price_with_discount = price - (price * discount / 100)
     #return price_with_discount
 
-#print(discounted(100, 50, max_discount=100))
+#print(discounted(100, 50, max_discount=100)) # давай ещё форматер сделаем красивый? Из серии «только сегодня скидка х и вместо н цена будет всего а»
+# и общую функцию, а-ля сформируй ценник
 
 
